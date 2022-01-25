5e52b116dbfeef4567a2bc8ab9da4c58b14c4b23
Carter Jernigan <git@carterjernigan.com>
2022-01-25 16:01:34 -0500

    Configure build with GitHub Actions

--- build-convention/src/main/kotlin/zcash.dependency-conventions.gradle.kts
+++ build-convention/src/main/kotlin/zcash.dependency-conventions.gradle.kts
@@ -2,22 +2,8 @@ dependencyLocking {
     lockAllConfigurations()
 }
 
-// Just resolves dependencies
 tasks {
     register("resolveAll") {
-        doLast {
-            configurations.filter {
-                // Add any custom filtering on the configurations to be resolved
-                it.isCanBeResolved
-            }.forEach { it.resolve() }
-        }
-    }
-
-// Invoke with `./gradlew resolveAndLockAll --write-locks`
-    register("resolveAndLockAll") {
-        doFirst {
-            require(gradle.startParameter.isWriteDependencyLocks)
-        }
         doLast {
             configurations.filter {
                 // Add any custom filtering on the configurations to be resolved
