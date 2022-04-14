a069d23cc57cae98946b29bfb65d47863cd677e1
Honza <rychnovsky.honza@gmail.com>
2022-04-14 17:57:49 +0200

    [#356] Crash on Verify Your Backup while app goes to background.
    - Added Parcelable to the Index model class.

--- spackle-lib/src/main/java/co/electriccoin/zcash/spackle/model/Index.kt
+++ spackle-lib/src/main/java/co/electriccoin/zcash/spackle/model/Index.kt
@@ -1,12 +1,16 @@
 package co.electriccoin.zcash.spackle.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 /**
  * Useful for accessing arrays or lists by index.
  *
  * @param value A 0-based index.  Must be >= 0
  */
 @JvmInline
-value class Index(val value: Int) {
+@Parcelize
+value class Index(val value: Int): Parcelable {
     init {
         require(value >= 0) { "Index must be >= 0 but actually is $value" }
     }
