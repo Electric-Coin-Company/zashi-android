1a61d9c403f190318dd990b64f5913d67048e119
Honza <rychnovsky.honza@gmail.com>
2022-05-12 10:49:10 +0200

    Add comment with suggestion on how to refactor UpdateInfo class.

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/update/model/UpdateInfo.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/update/model/UpdateInfo.kt
@@ -3,6 +3,14 @@ package co.electriccoin.zcash.ui.screen.update.model
 import co.electriccoin.zcash.ui.screen.update.AppUpdateChecker
 import com.google.android.play.core.appupdate.AppUpdateInfo
 
+// UpdateInfo can be refactored once to have stronger representation invariants
+// (eliminate the null, priority + failed state probably doesn't have much meaning, etc).
+//
+// sealed class UpdateInfo {
+//     data class Success(priority, info, state) : UpdateInfo()
+//     object Failed : UpdateInfo()
+// }
+
 data class UpdateInfo(
     val priority: AppUpdateChecker.Priority,
     val isForce: Boolean,
