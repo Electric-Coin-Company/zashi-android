64ec46a4e4dc2bff43b1bfd0c0960101d753c78d
Carter Jernigan <git@carterjernigan.com>
2022-05-31 10:12:18 -0400

    5 seconds

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/Constants.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/Constants.kt
@@ -3,4 +3,4 @@ package co.electriccoin.zcash.ui.common
 import kotlin.time.Duration.Companion.seconds
 
 // Recommended timeout for Android configuration changes to keep Kotlin Flow from restarting
-val ANDROID_STATE_FLOW_TIMEOUT = 5000.seconds
+val ANDROID_STATE_FLOW_TIMEOUT = 5.seconds
