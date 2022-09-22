73ca68effdbd7030ffad59ccc4dc5c7b3e5d5777
Alexej Hlinka <gasstan@gmail.com>
2022-09-22 15:41:22 +0200

    added test for Seed screen

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/seed/AndroidSeed.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/seed/AndroidSeed.kt
@@ -25,7 +25,6 @@ private fun WrapSeed(
     activity: ComponentActivity,
     goBack: () -> Unit
 ) {
-    SecureScreen()
     val walletViewModel by activity.viewModels<WalletViewModel>()
 
     val persistableWallet = run {
