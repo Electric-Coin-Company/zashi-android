4021d47f578420b90bc4e5755d38416475c9bf57
Carter Jernigan <git@carterjernigan.com>
2022-11-22 08:03:22 -0500

    Remove `run` block

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/backup/ext/BackupStateExt.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/backup/ext/BackupStateExt.kt
@@ -7,19 +7,17 @@ import co.electriccoin.zcash.ui.screen.backup.state.BackupState
 private const val KEY_STAGE = "stage" // $NON-NLS
 
 internal val BackupState.Companion.Saver
-    get() = run {
-        mapSaver(
-            save = { it.toSaverMap() },
-            restore = {
-                if (it.isEmpty()) {
-                    BackupState()
-                } else {
-                    val stage = BackupStage.values[it[KEY_STAGE] as Int]
-                    BackupState(stage)
-                }
+    get() = mapSaver(
+        save = { it.toSaverMap() },
+        restore = {
+            if (it.isEmpty()) {
+                BackupState()
+            } else {
+                val stage = BackupStage.values[it[KEY_STAGE] as Int]
+                BackupState(stage)
             }
-        )
-    }
+        }
+    )
 
 private fun BackupState.toSaverMap() = buildMap {
     put(KEY_STAGE, current.value.order)
