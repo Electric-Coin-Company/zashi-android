71072f747ca9fe871178f08b467e53acf69c7b6b
Honza <rychnovsky.honza@gmail.com>
2023-02-09 10:42:48 +0100

    Add Android min SDK API level check test

--- app/src/androidTest/java/co/electriccoin/zcash/app/AndroidApiTest.kt
+++ app/src/androidTest/java/co/electriccoin/zcash/app/AndroidApiTest.kt
@@ -6,9 +6,10 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.SmallTest
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.lessThanOrEqualTo
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class TargetApiTest {
+class AndroidApiTest {
 
     @Test
     @SmallTest
@@ -19,6 +20,18 @@ class TargetApiTest {
         assertThat(
             ApplicationProvider.getApplicationContext<Application>().applicationInfo.targetSdkVersion,
             lessThanOrEqualTo(Build.VERSION_CODES.TIRAMISU)
+        )
+    }
+
+    @Test
+    @SmallTest
+    fun checkMinApi() {
+        // This test case prevents accidental release of the app with a different API level than we
+        // have currently set in gradle.properties. It could impact the app's functionality. Don't
+        // change this unless you're absolutely sure we're ready to set a new API level.
+        assertEquals(
+            ApplicationProvider.getApplicationContext<Application>().applicationInfo.minSdkVersion,
+            Build.VERSION_CODES.O_MR1
         )
     }
 }
