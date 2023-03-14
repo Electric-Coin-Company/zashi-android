6c798f90103c38ddb53312f582f0d8feb0d8650c
Honza <rychnovsky.honza@gmail.com>
2023-03-14 15:25:16 +0100

    [#804][Design system] Paddings - Dimens
    - Initial commit to ensure it meets our requirements
    - This solution is now in parallel with a simpler existing Paddings solution
    - Four spacing groups defined
    - Provides a way to define default and custom spacing values
    - Distinguished by screen size, but other metrics also available (e.g. orientation, aspect ratio, layout direction or screen shape)

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/theme/ZcashTheme.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/theme/ZcashTheme.kt
@@ -6,12 +6,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import co.electriccoin.zcash.ui.design.theme.internal.DarkColorPalette
 import co.electriccoin.zcash.ui.design.theme.internal.DarkExtendedColorPalette
+import co.electriccoin.zcash.ui.design.theme.internal.Dimens
 import co.electriccoin.zcash.ui.design.theme.internal.ExtendedTypography
 import co.electriccoin.zcash.ui.design.theme.internal.LightColorPalette
 import co.electriccoin.zcash.ui.design.theme.internal.LightExtendedColorPalette
+import co.electriccoin.zcash.ui.design.theme.internal.LocalDimens
 import co.electriccoin.zcash.ui.design.theme.internal.LocalExtendedColors
 import co.electriccoin.zcash.ui.design.theme.internal.LocalExtendedTypography
 import co.electriccoin.zcash.ui.design.theme.internal.Paddings
+import co.electriccoin.zcash.ui.design.theme.internal.ProvideDimens
 import co.electriccoin.zcash.ui.design.theme.internal.Typography
 
 @Composable
@@ -32,11 +35,13 @@ fun ZcashTheme(
     }
 
     CompositionLocalProvider(LocalExtendedColors provides extendedColors) {
-        MaterialTheme(
-            colorScheme = baseColors,
-            typography = Typography,
-            content = content
-        )
+        ProvideDimens {
+            MaterialTheme(
+                colorScheme = baseColors,
+                typography = Typography,
+                content = content
+            )
+        }
     }
 }
 
@@ -49,6 +54,10 @@ object ZcashTheme {
     val typography: ExtendedTypography
         @Composable
         get() = LocalExtendedTypography.current
+
+    val dimens: Dimens
+        @Composable
+        get() = LocalDimens.current
 
     val paddings: Paddings
         @Composable
