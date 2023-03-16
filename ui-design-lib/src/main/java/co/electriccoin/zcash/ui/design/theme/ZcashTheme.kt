5cfa3a99e3857e2d5e31494ec70f0a54fd3947ae
Honza Rychnovsky <rychnovsky.honza@gmail.com>
2023-03-16 13:23:55 +0100

    [#804][Design system] Paddings - Dimens
    * [#804][Design system] Paddings - Dimens
    
    - Initial commit to ensure it meets our requirements
    - This solution is now in parallel with a simpler existing Paddings solution
    - Provides a way to define default and custom spacing values
    - Could be distinguished by a screen size, but other metrics also available (e.g. orientation, aspect ratio, layout direction or screen shape)
    
    * Move spacings change logic to comment
    
    - We've decided to have only one regular spacing group for now, which is suitable for most of current phone devices
    
    * Move Dimens out of internal package
    
    * Link TODO for later Paddings remove
    
    * Link issue of Use Dimens across the app to TODO

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/theme/ZcashTheme.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/theme/ZcashTheme.kt
@@ -32,11 +32,13 @@ fun ZcashTheme(
     }
 
     CompositionLocalProvider(LocalExtendedColors provides extendedColors) {
-        MaterialTheme(
-            colorScheme = baseColors,
-            typography = Typography,
-            content = content
-        )
+        ProvideDimens {
+            MaterialTheme(
+                colorScheme = baseColors,
+                typography = Typography,
+                content = content
+            )
+        }
     }
 }
 
@@ -50,6 +52,14 @@ object ZcashTheme {
         @Composable
         get() = LocalExtendedTypography.current
 
+    // TODO [#808]: [Design system] Use Dimens across the app
+    // TODO [#808]: https://github.com/zcash/secant-android-wallet/issues/808
+    val dimens: Dimens
+        @Composable
+        get() = LocalDimens.current
+
+    // TODO [#807]: [Design system] Remove deprecated Paddings class
+    // TODO [#807]: https://github.com/zcash/secant-android-wallet/issues/807
     val paddings: Paddings
         @Composable
         get() = Paddings()
