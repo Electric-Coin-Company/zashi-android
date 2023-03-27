7ef83be18cf6084a4dbd0f719161f08c86319860
Honza <rychnovsky.honza@gmail.com>
2023-03-27 16:15:51 +0200

    Let SendArgumentsWrapperFixture use SDK fixtures

--- ui-lib/src/androidTest/java/co/electriccoin/zcash/ui/fixture/SendArgumentsWrapperFixture.kt
+++ ui-lib/src/androidTest/java/co/electriccoin/zcash/ui/fixture/SendArgumentsWrapperFixture.kt
@@ -1,13 +1,17 @@
 package co.electriccoin.zcash.ui.fixture
 
+import cash.z.ecc.android.sdk.fixture.WalletFixture
 import cash.z.ecc.android.sdk.model.Zatoshi
+import cash.z.ecc.android.sdk.model.ZcashNetwork
 import cash.z.ecc.android.sdk.model.toZecString
+import cash.z.ecc.sdk.fixture.MemoFixture
+import cash.z.ecc.sdk.fixture.ZatoshiFixture
 import co.electriccoin.zcash.ui.screen.send.model.SendArgumentsWrapper
 
 internal object SendArgumentsWrapperFixture {
-    const val RECIPIENT_ADDRESS = "tmEjY6KfCryQhJ1hKSGiA7p8EeVggpvN78r"
-    const val MEMO = "Thanks for lunch"
-    val AMOUNT = Zatoshi(123)
+    val RECIPIENT_ADDRESS = WalletFixture.Alice.getAddresses(ZcashNetwork.Testnet).unified
+    val MEMO = MemoFixture.new("Thanks for lunch").value
+    val AMOUNT = ZatoshiFixture.new(1)
     fun amountToFixtureZecString(amount: Zatoshi?) = amount?.toZecString()
 
     fun new(
