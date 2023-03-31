0c0ed5f07f1ba660d7cdb1f09b566043fbe9c425
Honza <rychnovsky.honza@gmail.com>
2023-03-31 11:30:28 +0200

    Restore screens UI enhance

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/component/TextField.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/component/TextField.kt
@@ -1,5 +1,6 @@
 package co.electriccoin.zcash.ui.design.component
 
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextField
@@ -8,6 +9,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.input.KeyboardType
 
 @Suppress("LongParameterList")
@@ -23,7 +25,9 @@ fun FormTextField(
     keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
     colors: TextFieldColors = TextFieldDefaults.textFieldColors(
         containerColor = Color.Transparent
-    )
+    ),
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    shape: Shape = TextFieldDefaults.filledShape
 ) {
     TextField(
         value = value,
@@ -33,6 +37,8 @@ fun FormTextField(
         colors = colors,
         modifier = modifier,
         leadingIcon = leadingIcon,
-        trailingIcon = trailingIcon
+        trailingIcon = trailingIcon,
+        keyboardActions = keyboardActions,
+        shape = shape
     )
 }
