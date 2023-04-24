a355865433e7a8a1748f549b9aa4cbdbdf047f02
Carter Jernigan <git@carterjernigan.com>
2023-04-24 15:34:50 -0400

    Kover 0.7.0-Beta

--- build-conventions-secant/src/main/kotlin/secant.kover-conventions.gradle.kts
+++ build-conventions-secant/src/main/kotlin/secant.kover-conventions.gradle.kts
@@ -1,16 +1,20 @@
 pluginManager.withPlugin("org.jetbrains.kotlinx.kover") {
     extensions.findByType<kotlinx.kover.gradle.plugin.dsl.KoverProjectExtension>()?.apply {
-        disabledForProject = !project.property("IS_KOTLIN_TEST_COVERAGE_ENABLED").toString().toBoolean()
+        if (!project.property("IS_KOTLIN_TEST_COVERAGE_ENABLED").toString().toBoolean()) {
+            disable()
+        }
     }
 
     extensions.findByType<kotlinx.kover.gradle.plugin.dsl.KoverReportExtension>()?.apply {
-        html {
-            onCheck = true
-            setReportDir(layout.buildDirectory.dir("kover/html"))
-        }
-        xml {
-            onCheck = true
-            setReportFile(layout.buildDirectory.file("kover/xml/report.xml"))
+        defaults {
+            html {
+                onCheck = true
+                setReportDir(layout.buildDirectory.dir("kover/html"))
+            }
+            xml {
+                onCheck = true
+                setReportFile(layout.buildDirectory.file("kover/xml/report.xml"))
+            }
         }
     }
 }
