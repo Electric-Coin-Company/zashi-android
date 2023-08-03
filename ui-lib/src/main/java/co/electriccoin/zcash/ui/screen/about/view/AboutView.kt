4ef205d5d18638b0d938e198d7e9179afb01eb9f
Honza <rychnovsky.honza@gmail.com>
2023-08-03 14:39:40 +0200

    Fix ktlint warning

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/about/view/AboutView.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/about/view/AboutView.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
