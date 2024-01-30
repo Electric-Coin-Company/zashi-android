2fd129c1804a3ad47f88f1c8b5c1297293446927
Honza <rychnovsky.honza@gmail.com>
2024-01-30 12:11:32 -0800

    [#1210] Release 0.2.0 (541)

--- ui-lib/src/androidTest/java/co/electriccoin/zcash/ui/screen/send/ext/WalletAddressExtTest.kt
+++ ui-lib/src/androidTest/java/co/electriccoin/zcash/ui/screen/send/ext/WalletAddressExtTest.kt
@@ -3,7 +3,6 @@ package co.electriccoin.zcash.ui.screen.send.ext
 import androidx.test.filters.SmallTest
 import cash.z.ecc.android.sdk.fixture.WalletAddressFixture
 import co.electriccoin.zcash.ui.test.getAppContext
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -11,28 +10,26 @@ import kotlin.test.assertEquals
 class WalletAddressExtTest {
     @Test
     @SmallTest
-    @OptIn(ExperimentalCoroutinesApi::class)
     fun testAbbreviatedSaplingAddress() =
         runTest {
             val actual = WalletAddressFixture.sapling().abbreviated(getAppContext())
 
             // TODO [#248]: The expected value should probably be reversed if the locale is RTL
             // TODO [#248]: https://github.com/Electric-Coin-Company/zashi-android/issues/248
-            val expected = "zs1hf…skt4u"
+            val expected = "zs1vp…ee5k0"
 
             assertEquals(expected, actual)
         }
 
     @Test
     @SmallTest
-    @OptIn(ExperimentalCoroutinesApi::class)
     fun testAbbreviatedTransparentAddress() =
         runTest {
             val actual = WalletAddressFixture.transparent().abbreviated(getAppContext())
 
             // TODO [#248]: The expected value should probably be reversed if the locale is RTL
             // TODO [#248]: https://github.com/Electric-Coin-Company/zashi-android/issues/248
-            val expected = "t1QZM…3CSPK"
+            val expected = "t1dRJ…1QmGz"
 
             assertEquals(expected, actual)
         }
