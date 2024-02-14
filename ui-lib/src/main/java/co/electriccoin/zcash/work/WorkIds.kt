b1c457bc113edc9d58fb4ad1425bfeec773dcb6f
Honza <rychnovsky.honza@gmail.com>
2024-02-14 13:31:56 +0100

    [#1151] Background syncing
    - A periodic background block synchronization has been added. When the device is connected to the internet using an unmetered connection and is plugged into the power, the background task will start to synchronize blocks randomly between 3 and 4 a.m.
    - The background worker was in place but not fully working, plus was set to trigger randomly in 24 hours
    - Changelog update
    - Closes #1151
    - Closes #634
    - Its follow-up #1249

--- ui-lib/src/main/java/co/electriccoin/zcash/work/WorkIds.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/work/WorkIds.kt
@@ -3,6 +3,7 @@ package co.electriccoin.zcash.work
 import android.content.Context
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.WorkManager
+import co.electriccoin.zcash.spackle.Twig
 
 object WorkIds {
     const val WORK_ID_BACKGROUND_SYNC = "co.electriccoin.zcash.background_sync"
@@ -10,11 +11,23 @@ object WorkIds {
     fun enableBackgroundSynchronization(context: Context) {
         val workManager = WorkManager.getInstance(context)
 
+        Twig.debug {
+            "BG Sync: existing work details:" +
+                " ${workManager.getWorkInfosForUniqueWork(WORK_ID_BACKGROUND_SYNC).get()}"
+        }
+
+        // Note: Re-enqueuing existing work is okay. Another approach would be to validate the existing work and
+        // enqueue it if it is not planned yet or not in a valid state
         workManager.enqueueUniquePeriodicWork(
             WORK_ID_BACKGROUND_SYNC,
             ExistingPeriodicWorkPolicy.KEEP,
             SyncWorker.newWorkRequest()
         )
+
+        Twig.debug {
+            "BG Sync: newly enqueued work details:" +
+                " ${workManager.getWorkInfosForUniqueWork(WORK_ID_BACKGROUND_SYNC).get()}"
+        }
     }
 
     fun disableBackgroundSynchronization(context: Context) {
