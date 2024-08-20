8cf586f27b1925b5bda54a322434f0b13fd86f8c
Honza <rychnovsky.honza@gmail.com>
2024-08-20 18:29:26 +0200

    Fix test fixtures

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/fixture/ExchangeRateStateFixture.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/fixture/ExchangeRateStateFixture.kt
@@ -3,5 +3,7 @@ package co.electriccoin.zcash.ui.fixture
 import co.electriccoin.zcash.ui.common.wallet.ExchangeRateState
 
 object ExchangeRateStateFixture {
-    fun new() = ExchangeRateState.OptedOut
+    val STATE = ExchangeRateState.OptedOut
+
+    fun new(state: ExchangeRateState = STATE) = state
 }
--- ui-lib/src/main/java/co/electriccoin/zcash/ui/fixture/ObserveFiatCurrencyResultFixture.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/fixture/ObserveFiatCurrencyResultFixture.kt
@@ -5,14 +5,18 @@ import co.electriccoin.zcash.ui.common.wallet.ExchangeRateState
 import kotlinx.datetime.Clock
 
 object ObserveFiatCurrencyResultFixture {
+    const val IS_LOADING: Boolean = true
+    const val IS_STALE: Boolean = false
+    const val IS_REFRESH_ENABLED: Boolean = false
+    val CURRENCY_CONVERSION: FiatCurrencyConversion = FiatCurrencyConversion(
+        timestamp = Clock.System.now(),
+        priceOfZec = 25.0
+    )
+
     fun new(
-        isLoading: Boolean = true,
-        isStale: Boolean = false,
-        isRefreshEnabled: Boolean = true,
-        currencyConversion: FiatCurrencyConversion? =
-            FiatCurrencyConversion(
-                timestamp = Clock.System.now(),
-                priceOfZec = 25.0
-            ),
+        isLoading: Boolean = IS_LOADING,
+        isStale: Boolean = IS_STALE,
+        isRefreshEnabled: Boolean = IS_REFRESH_ENABLED,
+        currencyConversion: FiatCurrencyConversion? = CURRENCY_CONVERSION,
     ) = ExchangeRateState.Data(isLoading, isStale, isRefreshEnabled, currencyConversion) {}
 }
