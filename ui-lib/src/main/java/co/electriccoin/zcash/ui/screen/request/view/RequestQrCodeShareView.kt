311d59beda1d8ec924a89e7fce44e5fc54e54215
Honza <rychnovsky.honza@gmail.com>
2024-10-11 21:33:32 +0200

    Separate Request screens to their files

---
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/request/view/RequestQrCodeShareView.kt
@@ -0,0 +1,22 @@
+package co.electriccoin.zcash.ui.screen.request.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import co.electriccoin.zcash.ui.design.theme.ZcashTheme
+import co.electriccoin.zcash.ui.screen.request.model.RequestState
+
+@Composable
+internal fun RequestQrCodeView(
+    state: RequestState.QrCode,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier) {
+        Spacer(Modifier.height(ZcashTheme.dimens.spacingDefault))
+
+        Text(text = "Memo")
+    }
+}
