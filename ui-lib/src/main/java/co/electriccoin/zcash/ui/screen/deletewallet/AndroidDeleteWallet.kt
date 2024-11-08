1cca11ee151038edf82f3768a6028ecd30c17656
Milan Cerovsky <milan@z.cash>
2024-11-08 15:13:10 +0100

    Wallet deletion navigation navigation update

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/deletewallet/AndroidDeleteWallet.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/deletewallet/AndroidDeleteWallet.kt
@@ -1,6 +1,7 @@
 package co.electriccoin.zcash.ui.screen.deletewallet
 
 import android.app.Activity
+import android.content.Intent
 import androidx.activity.compose.BackHandler
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
@@ -8,7 +9,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import co.electriccoin.zcash.di.koinActivityViewModel
-import co.electriccoin.zcash.spackle.Twig
 import co.electriccoin.zcash.ui.MainActivity
 import co.electriccoin.zcash.ui.R
 import co.electriccoin.zcash.ui.common.model.TopAppBarSubTitleState
@@ -54,20 +54,20 @@ internal fun WrapDeleteWallet(
         snackbarHostState = snackbarHostState,
         onBack = goBack,
         onConfirm = {
-            scope.launch {
-                walletViewModel.deleteWalletFlow(activity).collect { isWalletDeleted ->
-                    if (isWalletDeleted) {
-                        onConfirm()
-                        Twig.info { "Wallet deleted successfully" }
-                        // The app flows move to the Onboarding screens reactively
-                    } else {
-                        Twig.error { "Wallet deletion failed" }
+            walletViewModel.deleteWallet(
+                onSuccess = {
+                    onConfirm()
+                    activity.finish()
+                    activity.startActivity(Intent(activity, MainActivity::class.java))
+                },
+                onError = {
+                    scope.launch {
                         snackbarHostState.showSnackbar(
                             message = activity.getString(R.string.delete_wallet_failed)
                         )
                     }
                 }
-            }
+            )
         },
         topAppBarSubTitleState = topAppBarSubTitleState,
     )
