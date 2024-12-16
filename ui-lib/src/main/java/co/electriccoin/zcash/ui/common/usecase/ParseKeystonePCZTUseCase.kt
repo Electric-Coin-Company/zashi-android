f490846240a8d07e5ae30d3a84b1208587a94cee
Milan Cerovsky <milan@z.cash>
2024-12-16 13:51:40 +0100

    PCZT progress calculation update

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/ParseKeystonePCZTUseCase.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/ParseKeystonePCZTUseCase.kt
@@ -5,15 +5,17 @@ import co.electriccoin.zcash.ui.NavigationRouter
 import co.electriccoin.zcash.ui.common.repository.KeystoneProposalRepository
 import co.electriccoin.zcash.ui.screen.transactionprogress.KeystoneTransactionProgress
 import com.keystone.module.DecodeResult
-import com.keystone.sdk.KeystoneSDK
+import com.sparrowwallet.hummingbird.ResultType
+import com.sparrowwallet.hummingbird.URDecoder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.math.min
 
 class ParseKeystonePCZTUseCase(
     private val keystoneProposalRepository: KeystoneProposalRepository,
     private val navigationRouter: NavigationRouter
 ) {
-    private val keystoneSDK = KeystoneSDK()
+    private val keystoneDecoder = KeystoneDecoder()
 
     suspend operator fun invoke(result: String): ParseKeystoneQrResult =
         withContext(Dispatchers.Default) {
@@ -40,7 +42,7 @@ class ParseKeystonePCZTUseCase(
 
     private fun decodeResult(result: String): DecodeResult {
         return try {
-            keystoneSDK.decodeQR(result)
+            keystoneDecoder.decodeQR(result)
         } catch (_: Exception) {
             throw InvalidKeystonePCZTQRException()
         }
@@ -48,3 +50,36 @@ class ParseKeystonePCZTUseCase(
 }
 
 class InvalidKeystonePCZTQRException : Exception()
+
+private class KeystoneDecoder {
+    private var urDecoder: URDecoder = URDecoder()
+
+    @Suppress("MagicNumber", "TooGenericExceptionThrown")
+    fun decodeQR(qr: String): DecodeResult {
+        val isReceived = urDecoder.receivePart(qr)
+        if (urDecoder.result == null) {
+            if (isReceived) {
+                val processedParts = urDecoder.processedPartsCount.toFloat()
+                val expectedParts = urDecoder.expectedPartCount.toFloat() * 1.75f
+                val progress = (processedParts / expectedParts) * 100
+                return DecodeResult(min(100, progress.toInt()))
+            } else {
+                resetQRDecoder()
+                throw Exception("Unexpected QR code")
+            }
+        }
+        when (urDecoder.result.type) {
+            ResultType.SUCCESS -> {
+                return DecodeResult(100, urDecoder.result.ur)
+            }
+            else -> {
+                resetQRDecoder()
+                throw Exception("Invalid QR code")
+            }
+        }
+    }
+
+    private fun resetQRDecoder() {
+        urDecoder = URDecoder()
+    }
+}
