fb750d2acff8f90086c22f1c87142247bf115fb4
Milan Cerovsky <milan@z.cash>
2024-12-18 13:23:59 +0100

    Code cleanup

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/util/KeystoneUtils.kt
+++
@@ -1,7 +0,0 @@
-package co.electriccoin.zcash.ui.util
-
-import com.keystone.sdk.KeystoneSDK
-import com.keystone.sdk.KeystoneZcashSDK
-
-val KeystoneSDK.zcash: KeystoneZcashSDK
-    get() = KeystoneZcashSDK()
