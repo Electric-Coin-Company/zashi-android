3d7c1c101f27b414718f8a97d0715e314497572a
Milan Cerovsky <milan@z.cash>
2024-12-18 13:28:42 +0100

    Code cleanup

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/ParseKeystonePCZTUseCase.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/ParseKeystonePCZTUseCase.kt
@@ -52,10 +52,11 @@ abstract class BaseKeystoneScanner {
                             )
                         } catch (e: Exception) {
                             keystoneSDK.resetQRDecoder()
-                            latestResult = ParseKeystoneQrResult(
-                                progress = 0,
-                                isFinished = false
-                            )
+                            latestResult =
+                                ParseKeystoneQrResult(
+                                    progress = 0,
+                                    isFinished = false
+                                )
                             throw e
                         }
                     } else {
