36febe6682cb96b19cbf92115dae9daa3889ebe7
Honza Rychnovský <honza@rychnovsky.dev>
2025-02-12 09:49:48 +0100

    [#1768] `Zashi Foss` build type (#1769)
    * [#1768] Zashi Foss build type
    
    - Closes #1768
    - Changelog updated
    
    * Support `zcashtestnetFoss` build type provider
    
    * Code cleanup
    
    * Build type converted to dimension
    
    * CI update
    
    * Code cleanup
    
    * Code cleanup
    
    Closes #1763
    
    * CI update
    
    * CI update
    
    ---------
    
    Co-authored-by: Milan Cerovsky <milan@z.cash>

---
+++ build-conventions-secant/src/main/kotlin/model/Dimensions.kt
@@ -0,0 +1,19 @@
+package model
+
+enum class NetworkDimension(val value: String) {
+    MAINNET("zcashmainnet"),
+    TESTNET("zcashtestnet");
+
+    companion object {
+        const val DIMENSION_NAME = "network"
+    }
+}
+
+enum class DistributionDimension(val value: String) {
+    STORE("store"),
+    FOSS("foss");
+
+    companion object {
+        const val DIMENSION_NAME = "distribution"
+    }
+}
