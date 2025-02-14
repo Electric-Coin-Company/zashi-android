f313805b4d58984b5712b4204700253045941e97
y4ssi <11613913+y4ssi@users.noreply.github.com>
2025-02-13 23:51:26 -0700

    Update build.gradle.kts

--- build-info-lib/build.gradle.kts
+++ build-info-lib/build.gradle.kts
@@ -16,8 +16,8 @@ private val gitCommitCountKey = "gitCommitCount"
 private val releaseNotesEn = "releaseNotesEn"
 private val releaseNotesEs = "releaseNotesEs"
 
-private val releaseNotesEnPath = "docs/whatsNew/WHATS_NEW_EN.md"
-private val releaseNotesEsPath = "docs/whatsNew/WHATS_NEW_ES.md"
+private val releaseNotesEnPath = file("${project.rootDir}/docs/whatsNew/WHATS_NEW_EN.md")
+private val releaseNotesEsPath = file("${project.rootDir}/docs/whatsNew/WHATS_NEW_ES.md")
 
 // Injects build information
 // Note timestamp is not currently injected because it effectively disables the cache since it
