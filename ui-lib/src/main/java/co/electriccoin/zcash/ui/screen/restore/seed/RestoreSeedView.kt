696344f8326026840a8d35937c9c6e7e4f4c3736
Milan Cerovsky <milan@z.cash>
2025-03-31 15:10:53 +0200

    Keyboard handling hotfix

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/restore/seed/RestoreSeedView.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/restore/seed/RestoreSeedView.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,14 +25,18 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -67,6 +73,18 @@ fun RestoreSeedView(
     suggestionsState: RestoreSeedSuggestionsState
 ) {
     val handle = rememberSeedTextFieldHandle(state.seed)
+
+    val focusManager = LocalFocusManager.current
+    var wasKeyboardOpen by remember { mutableStateOf(false) }
+    val isKeyboardOpen by rememberKeyboardState()
+
+    LaunchedEffect(isKeyboardOpen) {
+        if (wasKeyboardOpen && !isKeyboardOpen) {
+            focusManager.clearFocus(true)
+        }
+
+        wasKeyboardOpen = isKeyboardOpen
+    }
 
     BlankBgScaffold(
         topBar = { AppBar(state) },
@@ -258,6 +276,12 @@ private fun getFilteredSuggestions(
         suggestionsState.suggestions.contains(trimmed) && autocomplete.size == 1 -> autocomplete
         else -> autocomplete
     }
+}
+
+@Composable
+private fun rememberKeyboardState(): State<Boolean> {
+    val isImeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
+    return rememberUpdatedState(isImeVisible)
 }
 
 @PreviewScreens
