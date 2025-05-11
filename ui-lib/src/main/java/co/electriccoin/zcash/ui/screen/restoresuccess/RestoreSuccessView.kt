691a3bba3b56a98e7e166050636d5aee4eb9f70e
Milan Cerovsky <milan@z.cash>
2025-05-11 16:13:38 +0200

    Remove gradient background from the Restore instructions screen

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/restoresuccess/RestoreSuccessView.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/screen/restoresuccess/RestoreSuccessView.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import co.electriccoin.zcash.ui.R
+import co.electriccoin.zcash.ui.design.component.BlankBgScaffold
 import co.electriccoin.zcash.ui.design.component.BlankSurface
 import co.electriccoin.zcash.ui.design.component.GradientBgScaffold
 import co.electriccoin.zcash.ui.design.component.ZashiButton
@@ -40,10 +41,7 @@ import co.electriccoin.zcash.ui.design.util.stringRes
 
 @Composable
 fun RestoreSuccessView(state: RestoreSuccessViewState) {
-    GradientBgScaffold(
-        startColor = ZashiColors.Utility.WarningYellow.utilityOrange100,
-        endColor = ZashiColors.Surfaces.bgPrimary,
-    ) { paddingValues ->
+    BlankBgScaffold { paddingValues ->
         RestoreSuccessContent(
             state = state,
             modifier =
