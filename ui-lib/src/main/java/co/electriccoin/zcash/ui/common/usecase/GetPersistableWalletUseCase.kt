f720fe9414c3d401a26ba16c6a671282b34629fd
Milan Cerovsky <milan@z.cash>
2025-05-11 16:11:55 +0200

    Code cleanup

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/GetPersistableWalletUseCase.kt
+++
@@ -1,9 +0,0 @@
-package co.electriccoin.zcash.ui.common.usecase
-
-import co.electriccoin.zcash.ui.common.provider.PersistableWalletProvider
-
-class GetPersistableWalletUseCase(
-    private val persistableWalletProvider: PersistableWalletProvider
-) {
-    suspend operator fun invoke() = persistableWalletProvider.getPersistableWallet()
-}
