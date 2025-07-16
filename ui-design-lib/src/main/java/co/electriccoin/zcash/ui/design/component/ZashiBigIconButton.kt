7b14d7f37919062b4d02f1f47f2249638a38e9af
Milan Cerovsky <milan@z.cash>
2025-07-16 12:54:40 +0200

    Design updates

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/component/ZashiBigIconButton.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/component/ZashiBigIconButton.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,16 +16,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.input.pointer.changedToDown
-import androidx.compose.ui.input.pointer.changedToUp
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -44,7 +42,8 @@ fun ZashiBigIconButton(
     state: BigIconButtonState,
     modifier: Modifier = Modifier,
 ) {
-    var isPressed by remember { mutableStateOf(false) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
     val shadowElevation by animateDpAsState(if (isPressed) 0.dp else (2.dp orDark 4.dp))
 
     val darkBgGradient =
@@ -65,30 +64,15 @@ fun ZashiBigIconButton(
             Modifier.background(darkBgGradient)
 
     Surface(
-        modifier =
-            modifier
-                .pointerInput(Unit) {
-                    awaitPointerEventScope {
-                        while (true) {
-                            val event = awaitPointerEvent()
-                            event.changes.forEach { change ->
-                                if (change.changedToDown()) {
-                                    isPressed = true
-                                }
-                                if (change.changedToUp()) {
-                                    isPressed = false
-                                }
-                            }
-                        }
-                    }
-                },
+        modifier = modifier,
         onClick = state.onClick,
         color = ZashiColors.Surfaces.bgPrimary,
         shape = RoundedCornerShape(22.dp),
         border =
             BorderStroke(.5.dp, ZashiColors.Utility.Gray.utilityGray100) orDark
                 BorderStroke(.5.dp, darkBorderGradient),
-        shadowElevation = shadowElevation
+        shadowElevation = shadowElevation,
+        interactionSource = interactionSource
     ) {
         Column(
             modifier = backgroundModifier,
@@ -114,6 +98,7 @@ fun ZashiBigIconButton(
     }
 }
 
+@Immutable
 data class BigIconButtonState(
     val text: StringResource,
     @DrawableRes val icon: Int,
