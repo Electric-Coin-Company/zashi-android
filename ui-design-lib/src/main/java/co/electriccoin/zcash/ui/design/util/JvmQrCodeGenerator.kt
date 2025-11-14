8b048959f5845a58d72104d85ee6fbf5dac864ea
BostonZcash <55412725+BostonZcash@users.noreply.github.com>
2025-11-14 10:26:16 -0500

    fix: increase deposit QR error correction

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/util/JvmQrCodeGenerator.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/util/JvmQrCodeGenerator.kt
@@ -3,6 +3,7 @@ package co.electriccoin.zcash.ui.design.util
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 
 const val QR_CODE_IMAGE_MARGIN_IN_PIXELS = 2
 
@@ -14,7 +15,10 @@ object JvmQrCodeGenerator : QrCodeGenerator {
                 BarcodeFormat.QR_CODE,
                 sizePixels,
                 sizePixels,
-                mapOf(EncodeHintType.MARGIN to QR_CODE_IMAGE_MARGIN_IN_PIXELS)
+                mapOf(
+                    EncodeHintType.MARGIN to QR_CODE_IMAGE_MARGIN_IN_PIXELS,
+                    EncodeHintType.ERROR_CORRECTION to ErrorCorrectionLevel.M,
+                    )
             )
 
         return BooleanArray(sizePixels * sizePixels).apply {
