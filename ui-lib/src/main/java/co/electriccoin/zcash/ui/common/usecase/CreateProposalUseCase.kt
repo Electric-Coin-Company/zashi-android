034e76e0b041df57ef258e233960a88d99adbfc7
Milan <milan@z.cash>
2025-12-07 19:13:55 +0100

    Unsupported TEX hotfix

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/CreateProposalUseCase.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/CreateProposalUseCase.kt
@@ -1,22 +1,26 @@
 package co.electriccoin.zcash.ui.common.usecase
 
 import cash.z.ecc.android.sdk.model.ZecSend
+import cash.z.ecc.android.sdk.type.AddressType
 import cash.z.ecc.sdk.extension.floor
 import co.electriccoin.zcash.ui.NavigationRouter
 import co.electriccoin.zcash.ui.common.datasource.AccountDataSource
 import co.electriccoin.zcash.ui.common.datasource.InsufficientFundsException
 import co.electriccoin.zcash.ui.common.model.KeystoneAccount
 import co.electriccoin.zcash.ui.common.model.ZashiAccount
+import co.electriccoin.zcash.ui.common.provider.SynchronizerProvider
 import co.electriccoin.zcash.ui.common.repository.KeystoneProposalRepository
 import co.electriccoin.zcash.ui.common.repository.ZashiProposalRepository
 import co.electriccoin.zcash.ui.screen.insufficientfunds.InsufficientFundsArgs
 import co.electriccoin.zcash.ui.screen.reviewtransaction.ReviewTransactionArgs
+import co.electriccoin.zcash.ui.screen.texunsupported.TEXUnsupportedArgs
 
 class CreateProposalUseCase(
     private val keystoneProposalRepository: KeystoneProposalRepository,
     private val zashiProposalRepository: ZashiProposalRepository,
     private val accountDataSource: AccountDataSource,
-    private val navigationRouter: NavigationRouter
+    private val synchronizerProvider: SynchronizerProvider,
+    private val navigationRouter: NavigationRouter,
 ) {
     @Suppress("TooGenericExceptionCaught")
     suspend operator fun invoke(zecSend: ZecSend, floor: Boolean) {
@@ -24,13 +28,25 @@ class CreateProposalUseCase(
         try {
             when (accountDataSource.getSelectedAccount()) {
                 is KeystoneAccount -> {
-                    keystoneProposalRepository.createProposal(normalized)
-                    keystoneProposalRepository.createPCZTFromProposal()
+                    val destination = synchronizerProvider
+                        .getSynchronizer()
+                        .validateAddress(zecSend.destination.address)
+                    if (destination is AddressType.Tex) {
+                        throw TexUnsupportedOnKSException()
+                    } else {
+                        keystoneProposalRepository.createProposal(normalized)
+                        keystoneProposalRepository.createPCZTFromProposal()
+                    }
                 }
+
                 is ZashiAccount ->
                     zashiProposalRepository.createProposal(normalized)
             }
             navigationRouter.forward(ReviewTransactionArgs)
+        } catch (_: TexUnsupportedOnKSException) {
+            navigationRouter.forward(TEXUnsupportedArgs)
+            keystoneProposalRepository.clear()
+            zashiProposalRepository.clear()
         } catch (_: InsufficientFundsException) {
             keystoneProposalRepository.clear()
             zashiProposalRepository.clear()
@@ -42,3 +58,5 @@ class CreateProposalUseCase(
         }
     }
 }
+
+private class TexUnsupportedOnKSException : Exception("TEX addresses are unsupported on KS")
