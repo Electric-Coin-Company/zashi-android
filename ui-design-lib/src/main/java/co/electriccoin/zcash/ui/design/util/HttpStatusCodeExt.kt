b3e0a68bcb3b13c7c635950ff1efc68490bb382a
Milan <milan@z.cash>
2025-12-12 12:55:48 +0100

    [#2022] [MOB-45] Improve error handling behavior for 500 errors (v2)
    Closes #2022

--- ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/util/HttpStatusCodeExt.kt
+++ ui-design-lib/src/main/java/co/electriccoin/zcash/ui/design/util/HttpStatusCodeExt.kt
@@ -2,4 +2,5 @@ package co.electriccoin.zcash.ui.design.util
 
 import io.ktor.http.HttpStatusCode
 
+@Suppress("MagicNumber")
 fun HttpStatusCode.isServiceUnavailable(): Boolean = value in (501..504)
